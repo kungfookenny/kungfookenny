a58cf4d36a33d356068e59459f39901bc31f05e0
Brandon Still <kenny@kungfookenny.com>
2016-11-11 14:16:27 -0800

    cleanup

--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -4,62 +4,43 @@ var foundation = require('./vendor/foundation/js/foundation.min.js');
 // -- Init Foundation
 $(document).foundation();
 
-// Hide nav
-
-var mainHeader = $('.c-header'),
-  arrow = $('.o-bullet__arrow--animate'),
-  scrolling = false,
-  previousTop = 0,
-  currentTop = 0,
-  scrollDelta = 10,
-  scrollOffset = 150;
-
 $(window).on('scroll', function(){
-  if( !scrolling ) {
-    scrolling = true;
-    addNavBackround();
-    arrowAnimate();
-    (!window.requestAnimationFrame)
-      ? setTimeout(autoHideHeader, 250)
-      : requestAnimationFrame(autoHideHeader);
-  }
+  addNavBackround();
+  arrowAnimate();
 });
 
-function autoHideHeader() {
-  var currentTop = $(window).scrollTop();
-  checkSimpleNavigation(currentTop);
-  previousTop = currentTop;
-  scrolling = false;
-}
-
-function checkSimpleNavigation(currentTop) {
-  if (previousTop - currentTop > scrollDelta) {
-    mainHeader.removeClass('is--hidden');
-  } else if( currentTop - previousTop > scrollDelta && currentTop > scrollOffset) {
-    mainHeader.addClass('is--hidden');
-  }
-}
-
-function addNavBackround(currentTop) {
-  var currentTop = $(window).scrollTop();
+// Add background to main nav on scroll
+function addNavBackround() {
+  var currentTop = $(window).scrollTop(),
+    mainHeader = $('.c-header');
   if (currentTop > 0) {
     mainHeader.removeClass('c-header--top');
   } else {
     mainHeader.addClass('c-header--top');
   }
-}
+};
 
-function arrowAnimate(currentTop) {
-  var currentTop = $(window).scrollTop();
+// Animate arrow in hero section
+function arrowAnimate() {
+  var currentTop = $(window).scrollTop(),
+    arrow = $('.o-bullet__arrow--animate');
   if (currentTop > 0) {
     arrow.removeClass('o-bullet__arrow--animate');
   } else {
     arrow.addClass('o-bullet__arrow--animate');
   }
-}
+};
+
+// Hide project
+$('.o-project').each(function (e) {
+  if (e >= 1) $(this).hide();
+});
+
+// Show new project
+$('.o-project__moreButton').on('click', viewProject);
 
 // Show project
-viewProject = function (e) {   
+function viewProject(e) {   
   var wrapper = $(this).parent(),
     viewMore = 'view more',
     noMore = 'that\'s all!',
@@ -85,15 +66,7 @@ viewProject = function (e) {
   }
 };
 
-// Hide project
-$('.o-project').each(function (e) {
-  if (e >= 1) $(this).hide();
-});
-
-// Show new project
-$('.o-project__moreButton').on('click', viewProject);
-
-// -- Smooth scroll
+// Smooth scroll
 $('a[href*="#"]:not([href*="/#"])').click(function(e) {
   e.preventDefault();
   if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
