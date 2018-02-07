3844eff67e3b9c3dad7282b0f90a6818e05e4a2a
Brandon Still <brandon@Brandons-MacBook-Pro.local>
2018-02-06 21:29:33 -0800

    added logo toggle class for inner pages

--- src/scripts/components/c-header.js
+++ src/scripts/components/c-header.js
@@ -9,13 +9,16 @@
         var currentTop = $(window).scrollTop();
         var $hero = $('.c-homepage-hero');
         var headerHeight = $this.height();
+        var $logo = $('.c-header .o-brand');
         var logoHalfHeight = $hero.find('.o-brand').height() / 2;
         var heroHeight = $('.c-homepage-hero').height(); // move this
         var logoTrigger = heroHeight / 2 + logoHalfHeight - headerHeight;
         if (currentTop > 0) {
           $this.removeClass('c-header--top');
+          $logo.removeClass('o-brand--white');
         } else {
           $this.addClass('c-header--top');
+          $logo.addClass('o-brand--white');
         }
         if (currentTop < logoTrigger) {
           $this.removeClass('c-header--logo');
